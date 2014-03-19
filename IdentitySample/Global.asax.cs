4e363b3328b962498f458796f4641cca820de90f
igoryen <igor.yentaltsev@gmail.com>
2014-03-19 16:30:54 -0400

    first commit

---
+++ IdentitySample/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+// Added by MF
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+using IdentitySample.Models;
+
+namespace IdentitySample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+// Added by MF
+            Database.SetInitializer<MyDbContext>(new MyDbInitializer());
+        }
+    }
+}
